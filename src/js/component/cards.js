ca042500dbfa5212bb970b18b848bb88e1985b84
kedwards413 <kaela.rose.edwards@gmail.com>
2021-01-09 02:57:53 +0000

    edits

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -2,18 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 //include images into your bundle
 
+const cardStyles = {
+	width: "18rem",
+	margin: "20px"
+};
+
 export const Cards = props => {
 	return (
-		<div className="card" style = {{ width: "18rem" }}>
-			<img src={props.cardImage} className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">{props.description}</p>
-				<a href={props.buttonURL} className="btn btn-primary">
-					{props.buttonLabel}
-				</a>
+		<>
+			<div
+				className="card d-flex align-items-center flex-column"
+				style={cardStyles}>
+				<img src={props.cardImage} className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.description}</p>
+					<a href={props.buttonURL} className="btn btn-primary">
+						{props.buttonLabel}
+					</a>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -19,18 +19,45 @@ export const Home = props => {
 				<Jumbotron
 					title="A Warm Welcome!"
 					description="Thank you for visiting my first website using React"
-					buttonLabel="Click here to learn more about React!"
-					buttonURL="https:reactjs.org/"
+					buttonLabel="Click here to learn more about React"
+					buttonURL="https://reactjs.org/"
 				/>
 			</div>
 			<div>
 				<Cards
-					title="Favorite animal"
-					cardImage="https:i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg"
-					description="Test test test test test test"
-					buttonURL="https:en.wikipedia.org/wiki/Main_Page"
+					title="Hello!"
+					cardImage="https://i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg"
+					description="This is my first website using React. I hope you enjoy it. Thank you for stopping by!"
+					buttonURL="https://en.wikipedia.org/wiki/Main_Page"
 					buttonLabel="Click here"
 				/>
+			</div>
+			<div>
+				<Cards
+					title="Welcome!"
+					cardImage="https://i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg"
+					description="This is my first website using React. I hope you enjoy it. Thank you for stopping by!"
+					buttonURL="https://en.wikipedia.org/wiki/Main_Page"
+					buttonLabel="Click here"
+				/>
+			</div>
+			<div>
+				<Cards
+					title="Hello!"
+					cardImage="https://i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg"
+					description="This is my first website using React. I hope you enjoy it. Thank you for stopping by!"
+					buttonURL="https://en.wikipedia.org/wiki/Main_Page"
+					buttonLabel="Click here"
+				/>
+				<div>
+					<Cards
+						title="Card Header"
+						cardImage="https://i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg"
+						description="This is my first website using React. I hope you enjoy it. Thank you for stopping by!"
+						buttonURL="https://en.wikipedia.org/wiki/Main_Page"
+						buttonLabel="Click here"
+					/>
+				</div>
 			</div>
 		</div>
 	);
